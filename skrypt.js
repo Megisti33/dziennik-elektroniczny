c4d4646fff3fda4454eec1e94a09cfb941295f34
Megisti <105646520+Megisti33@users.noreply.github.com>
2024-05-15 22:02:07 +0200

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,18 @@
+// var anchor = document.querySelector("a");
+
+// import json_data from './some_file.json' assert {type: "json"};
+
+// //console.log(json_data)
+
+// var data = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(json_data));
+
+
+// anchor.addEventListener("click", ()=>
+//     {
+//         anchor.setAttribute("href", data);
+//         anchor.setAttribute("download", "export.json");
+//         anchor.setAttribute("href", "")
+//     }
+// );
+
+// anchor.addEventListener("change")
